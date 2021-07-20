b78b4274f7750a028b90eed0123d161c34841700
이규정 <rbwjd0101@naver.com>
2021-07-20 19:50:43 +0900

    FEATURE: signup page UI - add script code

--- frontend/src/views/Signup.vue
+++ frontend/src/views/Signup.vue
@@ -1,36 +1,56 @@
 <template>
   <div class="container d-flex justify-center">
     <div class="d-flex flex-column">
-      <img src="../../public/Tumo_Korean.png" alt="CI">
+      <img src="../../public/Tumo_Korean.png" alt="CI" width="300" class="mb-5">
       <h1 class="mb-3">가입하기</h1>
       <v-form
         ref="form"
         v-model="valid"
         lazy-validation
       >
+        <!-- 서비스 내 이름 -->
+        <v-text-field
+          v-model="name"
+          :rules="nameRules"
+          :counter="10"
+          label="Name"
+          required
+        ></v-text-field>
+
+        <!-- 이메일 계정 -->
         <v-text-field
           v-model="email"
           :rules="emailRules"
           label="E-mail"
           required
         ></v-text-field>
- 
+
+        <!-- 비밀번호 -->
         <v-text-field
-          v-model="name"
-          :counter="10"
-          :rules="nameRules"
-          label="Name"
+          v-model="password"
+          :rules="passwordRules"
+          label="Password"
           required
         ></v-text-field>
 
+        <!-- 비밀번호 확인 -->
+        <v-text-field
+          v-model="passwordConfirmation"
+          :rules="confirmationRules"
+          label="Password Confirmation"
+          required
+        ></v-text-field>
+
+        <!-- 관심 키워드 -->
         <v-select
           v-model="select"
-          :items="items"
           :rules="[v => !!v || 'Item is required']"
-          label="Item"
+          :keywords="keywords"
+          label="Keyword"
           required
         ></v-select>
 
+        <!-- 약관 동의 -->
         <v-checkbox
           v-model="checkbox"
           :rules="[v => !!v || 'You must agree to continue!']"
@@ -39,27 +59,13 @@
         ></v-checkbox>
 
         <v-btn
-          :disabled="!valid"
-          color="success"
           class="mr-4"
-          @click="validate"
+          @click="submit"
         >
-          Validate
+          submit
         </v-btn>
-
-        <v-btn
-          color="error"
-          class="mr-4"
-          @click="reset"
-        >
-          Reset Form
-        </v-btn>
-
-        <v-btn
-          color="warning"
-          @click="resetValidation"
-        >
-          Reset Validation
+        <v-btn @click="clear">
+          clear
         </v-btn>
       </v-form>
     </div>
@@ -67,15 +73,47 @@
 </template>
 
 <script>
-
   export default {
     name: 'signup',
     components: {
-    },
+    },    
     data: () => {
       return {
-        email: "", 
+        valid: true,
+        name: "",
+        nameRules: [
+        v => !!v || 'Name is required',
+        v => (v && v.length <= 10) || 'Name must be less than 10 characters',
+        ],
+        email: "",
+        emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+        ],
+        password: "",
+        passwordRules: [
+        v => !!v || 'password is required',
+        // v => /.+@.+\..+/.test(v) || 'E-mail must be valid', 비밀번호 규칙
+        ],
+        passwordConfirmation: "",
+        confirmationRules: [
+        v => !!v || '비밀번호 확인 is required',
+        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+        ],
+        select: null,
+        keywords: [
+          'Item 1',
+          'Item 2',
+          'Item 3',
+          'Item 4',
+        ],
+        checkbox: false,        
       }
-    }
+    },
+    methods: {
+      validate () {
+        this.$refs.form.validate()
+      }
+    },
   }
 </script>
