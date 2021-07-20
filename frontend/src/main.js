c34f75f53a22d3c69c08fdbe406ce4da0f035751
adj5672 <adj5672@naver.com>
2021-07-20 13:46:25 +0900

    FIXTURES: add bootstrap & logo

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './vuex/store'
-import vuetify from './plugins/vuetify'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./vuex/store";
+import vuetify from "./plugins/vuetify";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
-Vue.config.productionTip = false
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+Vue.config.productionTip = false;
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
