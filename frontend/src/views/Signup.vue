4e27f33615af820847834a9e60f8ee6a30710c6d
이규정 <rbwjd0101@naver.com>
2021-07-20 17:06:20 +0900

    FEATURE: signup page UI - CI, Input tag 삽입

---
+++ frontend/src/views/Signup.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="container d-flex justify-center">
+    <div class="d-flex flex-column">
+      <img src="../../public/Tumo_Korean.png" alt="CI">
+      <h1 class="mb-3">가입하기</h1>
+      <v-form
+        ref="form"
+        v-model="valid"
+        lazy-validation
+      >
+        <v-text-field
+          v-model="email"
+          :rules="emailRules"
+          label="E-mail"
+          required
+        ></v-text-field>
+ 
+        <v-text-field
+          v-model="name"
+          :counter="10"
+          :rules="nameRules"
+          label="Name"
+          required
+        ></v-text-field>
+
+        <v-select
+          v-model="select"
+          :items="items"
+          :rules="[v => !!v || 'Item is required']"
+          label="Item"
+          required
+        ></v-select>
+
+        <v-checkbox
+          v-model="checkbox"
+          :rules="[v => !!v || 'You must agree to continue!']"
+          label="Do you agree?"
+          required
+        ></v-checkbox>
+
+        <v-btn
+          :disabled="!valid"
+          color="success"
+          class="mr-4"
+          @click="validate"
+        >
+          Validate
+        </v-btn>
+
+        <v-btn
+          color="error"
+          class="mr-4"
+          @click="reset"
+        >
+          Reset Form
+        </v-btn>
+
+        <v-btn
+          color="warning"
+          @click="resetValidation"
+        >
+          Reset Validation
+        </v-btn>
+      </v-form>
+    </div>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'signup',
+    components: {
+    },
+    data: () => {
+      return {
+        email: "", 
+      }
+    }
+  }
+</script>
