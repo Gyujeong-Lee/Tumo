d60311ffca998df890dff5036c89dc2d4665aad2
adj5672 <adj5672@naver.com>
2021-07-22 10:51:50 +0900

    simple refactoring

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,9 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Login from "../views/Login.vue";
 import Signup from "../views/Signup.vue";
+import Main from "../views/Main.vue";
+import createArticle from "../views/createArticle.vue";
+import Profile from "../views/Profile.vue";
 
 Vue.use(VueRouter);
 
@@ -15,6 +18,21 @@ const routes = [
     path: "/signup",
     name: "signup",
     component: Signup,
+  },
+  {
+    path: "/main",
+    name: "main",
+    component: Main,
+  },
+  {
+    path: "/createArticle",
+    name: "createAritlce",
+    component: createArticle,
+  },
+  {
+    path: "/user/:userId",
+    name: "profile",
+    component: Profile,
   },
 ];
 
