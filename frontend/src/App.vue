611a9d08845118de370ccfea80bfa8546bb74b94
이규정 <rbwjd0101@naver.com>
2021-07-23 17:05:57 +0900

    FEATURE: add navbar component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <v-app>
     <v-main>
+      <!-- navbar는 jwt token 여부에 따라 토글한다. -->
+      <Navbar/>
       <router-view/>
     </v-main>
   </v-app>
 </template>
 
 <script>
+import Navbar from './components/layout/Navbar.vue'
 
 export default {
   name: 'App',
   data: () => ({
     //
   }),
-};
+  components: {
+    Navbar,
+  }
+}
 </script>
---
+++ frontend/src/components/layout/Navbar.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <v-app-bar
+    color="#00BFFE"
+    dense
+    dark
+    >
+      <div class="w-100 d-flex justify-space-between">
+        <!-- Logo -->
+        <router-link :to="{ name: 'Login' }"><img src="@/assets/Login/logo.png" alt="mainlogo" height="50"></router-link>
+        <!-- Search -->
+        <b-form-input class="mt-3" placeholder="Search">
+        </b-form-input> 
+        <!-- 아이콘 -->
+        <div class="mt-2">
+          <!-- 링크 수정할 것 -->
+          <v-btn icon>
+            <router-link :to="{ name: 'Login' }" style="text-decoration: none; color: inherit;"><v-icon>mdi-pencil</v-icon></router-link>
+          </v-btn>
+          <v-btn icon>
+            <router-link :to="{ name: 'Login' }" style="text-decoration: none; color: inherit;"><v-icon>mdi-apple-safari</v-icon></router-link>
+          </v-btn>
+          <v-btn icon>
+            <router-link :to="{ name: 'Login' }" style="text-decoration: none; color: inherit;"><v-icon>mdi-heart</v-icon></router-link>
+          </v-btn>
+          <v-btn icon>
+            <router-link :to="{ name: 'Login' }" style="text-decoration: none; color: inherit;"><v-icon>mdi-account-circle</v-icon></router-link>
+          </v-btn>
+        </div>
+
+      </div>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data: function () {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+  #nav-bar {
+    /* nav bar 색상 추가 */
+    background-color: #00BFFE;
+  }
+  input {
+    width: 20%;
+    height: 80%;
+  }
+</style>
