e256a2f2291093b58d4461558fd33a2a119f02e5
adj5672 <adj5672@naver.com>
2021-07-23 10:01:52 +0900

    FEATURE: create Main.vue

---
+++ frontend/src/components/Main/ArticleFeed.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="mb-4 p-3 border border-dark">
+    {{ data }}
+    <hr>
+    <div class="d-flex mb-3">
+      <div style="background-color: gray;">프로필</div>
+      <p class="my-0 mx-3">{{ data.user_idx }}</p>
+    </div>
+    <h5>{{ data.title }}</h5>
+    <p>{{ data.content }}</p>
+    <div class="d-flex justify-content-between">
+      <div>
+        <v-btn icon large v-if="islike" @click="cancelLikeArticle" color="error"><v-icon color="error">mdi-heart</v-icon></v-btn>
+        <v-btn icon large v-else @click="likeArticle"><v-icon>mdi-heart-outline</v-icon></v-btn>
+        <span>{{ likes }}</span>
+      </div>
+      <v-btn icon large><v-icon>mdi-comment-multiple-outline</v-icon></v-btn>
+      <div>
+        <v-btn icon large v-if="isscrap" @click="cancelScrapArticle"><v-icon>mdi-bookmark</v-icon></v-btn>
+        <v-btn icon large v-else @click="scrapArticle"><v-icon>mdi-bookmark-outline</v-icon></v-btn>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ArticleFeed',
+  props: {
+    data: {
+      type: Object
+    }
+  },
+  data: function () {
+    return {
+      islike: this.data.islike,
+      isscrap: this.data.isscrap,
+      likes: this.data.likes,
+    }
+  },
+  methods: {
+    likeArticle: function () {
+      this.islike = !this.islike
+      this.likes += 1
+      // axios 요청
+    },
+    cancelLikeArticle: function () {
+      this.islike = !this.islike
+      this.likes -= 1
+      // axios 요청
+    },
+    scrapArticle: function () {
+      this.isscrap = !this.isscrap
+      // axios 요청
+    },
+    cancelScrapArticle: function () {
+      this.isscrap = !this.isscrap
+      // axios 요청
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- frontend/src/views/Main.vue
+++ frontend/src/views/Main.vue
@@ -1,13 +1,50 @@
 <template>
-  <div></div>
+  <div id="mainPage" class="h-100">
+    <div class="d-flex justify-content-center h-100">
+      <div id="subfeed" class="border border-dark">
+        <p>123</p>
+      </div>
+      <div id="mainfeed">
+        <FeedList/>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import FeedList from '@/components/Main/FeedList'
+
 export default {
-  name: 'Main'
+  name: 'Main',
+  components: {
+    FeedList,
+  },
+  data: function () {
+    return {
+    }
+  }
 }
 </script>
 
 <style>
+@media screen and (min-width: 576px) {
+  #mainPage {
+    min-width: 930px;
+  }
+
+  #mainfeed {
+    width: 614px;
+    padding-left: 2rem;
+  }
+
+  #subfeed {
+    width: 300px;
+  }
+}
+
+#subfeed * {
+  position: sticky;
+  top: 0px;
+}
 
 </style>
