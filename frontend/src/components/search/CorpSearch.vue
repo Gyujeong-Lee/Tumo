6a799a620a798c667b18eacf750af357461cd8d5
이규정 <rbwjd0101@naver.com>
2021-08-04 16:05:21 +0900

    FEATURE: add searchResult, feed/portfolio 수정 예정

---
+++ frontend/src/components/search/CorpSearch.vue
@@ -0,0 +1,65 @@
+<template>
+  <div v-if="resultExist">
+    <h3>기업 검색 결과</h3>
+    <div class="d-flex flex-row my-autoss">
+      <v-card elevation="2" shaped v-for="(company, idx) in companies" :key="idx" class="me-3">
+        <v-card-title>{{ company.name }}</v-card-title>
+      </v-card>
+    </div>
+  </div>
+  <div v-else>
+    <h3>기업 검색 결과가 없습니다...</h3>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'CorpSearch',
+  data: function () {
+    return {
+      searchWord: this.searchItem,
+      resultExist: false,
+      companies: [
+        {
+            "code" : "1234",
+            "name" : "삼성중공업",
+            "industry" : "공업",
+            "type" : "KOSPI"
+        },
+        {
+            "code" : "1234",
+            "name" : "삼성전자",
+            "industry" : "전자",
+            "type" : "KOSPI"
+        }
+    ],
+  }
+},
+  props: {
+    searchItem: String,
+  },
+  created: function () {
+    // console.log(this.$route.params.keyword)
+    axios({
+      method: 'GET',
+      url: `/company/search/${this.$route.params.keyword}/1`
+    })
+    .then(res => {
+      // console.log(res)ㄴ
+      if (res.request.status === 204) {
+        console.log('자료 x')
+      } else {
+        this.resultExist = true
+        // 응답 담기
+        // this.companies = res.data.companies
+      }
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  },
+
+}
+</script>
---
+++ frontend/src/components/search/UserSearch.vue
@@ -0,0 +1,62 @@
+<template>
+  <div v-if="resultExist">
+    <h3>유저 검색 결과</h3>
+    <div class="d-flex flex-row my-autoss">
+      <v-card elevation="2" shaped v-for="(user, idx) in users" :key="idx" class="me-3">
+        <v-card-title>{{ user.nickName }}</v-card-title>
+      </v-card>
+    </div>
+  </div>
+  <div v-else>
+    <h3>유저 검색 결과가 없습니다...</h3>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'UserSearch',
+  data: function () {
+    return {
+      searchWord: this.searchItem,
+      resultExist: false,
+      users: [
+        {
+          "userIdx" : 1,
+          "nickName" : "이규정",
+          "introduce" : "안녕하세요.\n잘 부탁드립니다."
+        },
+        {
+          "userIdx" : 3,
+          "nickName" : "이규빈",
+          "introduce" : "안녕하세요.\n잘 부탁드립니다.22"
+        }
+      ],
+    }
+  },
+  props: {
+    searchItem: String,
+  },
+  created: function () {
+    axios({
+      method: 'GET',
+      url: `/sns/search/${this.$route.params.keyword}/1`
+    })
+    .then(res => {
+      // console.log(res)
+      if (res.request.status === 204) {
+        console.log('자료 x')
+      } else {
+        this.resultExist = true
+        // 응답 담기
+        // this.users = res.data.users
+      }
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  },
+
+}
+</script>
