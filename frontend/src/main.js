0a807871bba2545d2d52e1b895808680bf0dec25
adj5672 <adj5672@naver.com>
2021-08-06 15:33:46 +0900

    FIXTURE: update Article CRUD

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,16 +3,21 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./vuex/store";
 import vuetify from "./plugins/vuetify";
+import VueSimpleAlert from "vue-simple-alert";
+import { Popconfirm } from "element-ui";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
 import "remixicon/fonts/remixicon.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+import "element-ui/lib/theme-chalk/index.css";
 
 Vue.config.productionTip = false;
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
+Vue.use(Popconfirm);
+Vue.use(VueSimpleAlert);
 
 new Vue({
   router,
