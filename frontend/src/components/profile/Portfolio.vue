ea4509c4472cc5457632bfc1692b711f1adf926a
이규정 <rbwjd0101@naver.com>
2021-08-10 21:25:03 +0900

    FEATURE: profile 포트폴리오, article, scrap 출력 구현 완료

--- frontend/src/components/profile/Portfolio.vue
+++ frontend/src/components/profile/Portfolio.vue
@@ -1,4 +1,5 @@
 <template>
+<div>
   <v-sheet 
     :elevation="elevation"
     rounded
@@ -7,23 +8,62 @@
     @mouseleave="elevation=4"
     height="auto"
     width="auto">
-    <!-- 포문이 아니라 하나만 출력할 것임 -->
-    <p type="button" @click="moveToDetail(bestPortfolio.portfolio_idx)" class="bolder" style="color:#00BFFE; bolder">{{ bestPortfolio.title }} </p>
+    <h5 type="button" @click="moveToDetail(bestPortfolio.portfolio_idx)" >
+      {{ bestPortfolio.title }}
+    </h5>
+    <PortfolioChart v-if="assets.length && Object.keys(bestPortfolio).length" :portfolio="bestPortfolio" :assets="assets"/>
+    <div class="d-flex align-center justify-center mt-3">
+      <div class="d-flex flex-column border p-2" id="portfolioInfo">
+        <p>총 자산 : {{ amount.cursum }}원</p>
+        <p v-if="bestPortfolio.percent > 0" class="text-danger">현재 수익률 : {{ amount.percent }}%</p>
+        <p v-else class="text-primary">현재 수익률 : {{ amount.percent }}%</p>
+        <p >목표 수익률 : {{ bestPortfolio.goal }}%</p>
+      </div>
+    </div>
   </v-sheet>
+  <v-tabs grow class="p-0">
+    <v-tab grow @click="viewMore">
+    <span>더보기</span>
+   </v-tab>
+  </v-tabs>
+  <v-sheet
+  v-if="clickMore"
+  :elevation="elevation"
+  rounded
+  class="articleFeed mx-2 my-5"
+  @mouseover="elevation=10"
+  @mouseleave="elevation=4"
+  >
+    <div class="d-flex align-center mt-3">
+      <div class="d-flex flex-column p-2">
+        <h5 type="button" class="mb-3">다른 포트폴리오</h5>
+        <p type="button" v-for="(portfolio, idx) in portfolios" :key="idx" 
+        @click="moveToDetail(portfolio.portfolio_idx)">
+          {{ portfolio.title }}
+        </p>
+      </div>
+    </div>
+  </v-sheet>
+</div>
 </template>
 
 <script>
 import axios from 'axios'
 import * as _ from 'lodash'
+import PortfolioChart from  '@/components/portfolio/PortfolioChart.vue'
 
 export default {
   name: 'Portfolio',
   data: function () {
     return {
+      clickMore: false,
       userId: this.userIdx,
       elevation: 4,
-      // 이 중 하나 정해서 출력해야 함.
+      // 수익률이 가장 높은 포트폴리오
       bestPortfolio: {},
+      // best portfolio asset
+      assets: [],
+      amount: {},
       portfolios: [ 
         {
           "portfolio_idx":1,
@@ -42,6 +82,9 @@ export default {
       ],
     }
   },
+  components: {
+    PortfolioChart,
+  },
   props: {
     userIdx: Number,
   },
@@ -57,7 +100,20 @@ export default {
       this.portfolios = res.data.portfolio
       this.bestPortfolio = _.maxBy(res.data.portfolio, 'percent')
       console.log(this.bestPortfolio)
-
+      //여기부터 개별 자산
+      axios({
+        method: 'GET',
+        url: `/api/portfolio/asset/${this.bestPortfolio.portfolio_idx}`
+      })
+      .then(res=> {
+        console.log(res)
+        this.assets = res.data.Asset
+        this.amount = res.data.amount
+      })
+      .catch(err => {
+        console.log(err)
+      })
+      //여기까지
     })
     .catch(err => {
       console.log(err)
@@ -68,7 +124,19 @@ export default {
       console.log(this.userId)
       console.log(idx)
       this.$router.push({name: 'portfolioDetail', params: {userIdx: this.userId, portfolioIdx: idx}})
+    },
+    viewMore: function () {
+      this.clickMore = true
     }
   }
 }
 </script>
+
+<style scoped>
+  h5 {
+    color: #00BFFE;
+  }
+  p {
+    font-weight: bold;
+  }
+</style>
--- frontend/src/components/profile/activitylist/ScrapList.vue
+++ frontend/src/components/profile/activitylist/ScrapList.vue
@@ -1,13 +1,21 @@
 <template>
   <v-sheet 
-    :elevation="elevation"
-    rounded
-    class="articleFeed mx-2 my-5"
-    @mouseover="elevation=10"
-    @mouseleave="elevation=4"
-    height="auto"
-    width="auto">
-    <p class="p-3" type="button" @click="moveToDetail">{{ scrap.title }}</p>
+  :elevation="elevation"
+  rounded
+  class="articleFeed mx-2 my-5"
+  @mouseover="elevation=10"
+  @mouseleave="elevation=4"
+  height="auto"
+  width="auto">
+    <div>
+      <p style="font-weight:bold" type="button" @click="moveToDetail">{{ myScrap.title }}</p>
+      <div class="d-flex flex-row">
+        <img src="@/assets/main/user.png" alt="user_img" style="width: 35px;" class="me-3">
+        <router-link class="text-secondary nickname" :to="{ name: 'profile', params: { nickname: `${myScrap.nickname}` }}">
+          @{{ myScrap.nickname }}
+        </router-link>
+      </div>
+    </div>
   </v-sheet>
 </template>
 
