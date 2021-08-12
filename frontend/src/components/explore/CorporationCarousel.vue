d86edb61e833207154884e2d4a36c21e0fc67ea9
adj5672 <adj5672@naver.com>
2021-08-12 13:24:04 +0900

    FIX: fix user_info update & explore corporation

--- frontend/src/components/explore/CorporationCarousel.vue
+++ frontend/src/components/explore/CorporationCarousel.vue
@@ -10,14 +10,14 @@
         <img src="@/assets/explore/samsung.png" alt="samsung">
         <div>
           <h4>삼성전자(주)</h4>
-          <p style="font-size: 12px;">SAMSUNG ELECTRONICS CO,.LTD</p>
+          <p>SAMSUNG ELECTRONICS CO,.LTD</p>
         </div>
       </div>
       <div>
         <div class="my-2"></div>
         <a href="#">www.sec.co.kr</a>
-        <p style="font-size: 14px;">경기도 수원시 영통구 삼성로 129 (매탄동)</p>
-        <p style="font-size: 14px;">Tel: 031-200-1114</p>
+        <p>경기도 수원시 영통구 삼성로 129 (매탄동)</p>
+        <p>Tel: 031-200-1114</p>
       </div>
     </vue-glide-slide>
     <vue-glide-slide> 
@@ -25,14 +25,14 @@
         <img src="@/assets/explore/kakao.png" alt="kakao">
         <div>
           <h4>(주)카카오</h4>
-          <p style="font-size: 12px;">Kakao Corp.</p>
+          <p>Kakao Corp.</p>
         </div>
       </div>
       <div>
         <div class="my-2"></div>
         <a href="#">www.kakaocorp.com</a>
-        <p style="font-size: 14px;">제주특별자치도 제주시 첨단로 242</p>
-        <p style="font-size: 14px;">Tel: 02-6718-1082</p>
+        <p>제주특별자치도 제주시 첨단로 242</p>
+        <p>Tel: 02-6718-1082</p>
       </div>
     </vue-glide-slide>
     <vue-glide-slide> 
@@ -40,14 +40,44 @@
         <img src="@/assets/explore/naver.png" alt="naver">
         <div>
           <h4>네이버(주)</h4>
-          <p style="font-size: 12px;">NAVER Corporation</p>
+          <p>NAVER Corporation</p>
         </div>
       </div>
       <div>
         <div class="my-2"></div>
         <a href="#">www.navercorp.com</a>
-        <p style="font-size: 14px;">경기도 성남시 분당구 불정로 6</p>
-        <p style="font-size: 14px;">Tel: 031-784-4175</p>
+        <p>경기도 성남시 분당구 불정로 6</p>
+        <p>Tel: 031-784-4175</p>
+      </div>
+    </vue-glide-slide>
+    <vue-glide-slide> 
+      <div class="d-flex flex-column align-items-center">
+        <img src="@/assets/explore/hynix.png" alt="sk">
+        <div>
+          <h4>SK하이닉스(주)</h4>
+          <p>SK hynix Inc.</p>
+        </div>
+      </div>
+      <div>
+        <div class="my-2"></div>
+        <a href="#">www.skhynix.com</a>
+        <p>경기도 이천시 부발읍 경충대로 2091</p>
+        <p>Tel: 031-630-4114</p>
+      </div>
+    </vue-glide-slide>
+    <vue-glide-slide> 
+      <div class="d-flex flex-column align-items-center">
+        <img src="@/assets/explore/mobis.png" alt="mobis">
+        <div>
+          <h4>현대모비스(주)</h4>
+          <p>HYUNDAI MOBIS CO.,LTD</p>
+        </div>
+      </div>
+      <div>
+        <div class="my-2"></div>
+        <a href="#">www.mobis.co.kr</a>
+        <p>서울 강남구 역삼1동 로담코빌딩 140-2</p>
+        <p>Tel: 02-2018-5114</p>
       </div>
     </vue-glide-slide>
     <template slot="control">
@@ -68,9 +98,17 @@ export default {
       options: {
         type: "carousel",
         perView: 3,
-        gap: 0,
+        gap: 10,
         dragThreshold: false,
         focusAt: "center",
+        breakpoints: {
+          576: {
+            perView: 1
+          },
+          940: {
+            perView: 2
+          }
+        }
       }
     }
   },
@@ -110,11 +148,12 @@ export default {
         const title = slide.querySelector('h4')
         const color = this.getColor(logo)
         const rgb = `${color[0]}, ${color[1]}, ${color[2]}`
-        slide.style.border = `2px solid rgb(${rgb})`
+        slide.style.boxShadow = `inset 0px 0px 15px 0px rgba(${rgb}, 0.2)`
+        slide.style.border = `2px solid rgba(${rgb}, 0.3)`
         title.style.color = `rgb(${rgb})`
         slide.style.backgroundColor = `rgba(${rgb}, 0.1)`
       })
-    }, 300);
+    }, 500);
   }
 }
 </script>
@@ -124,21 +163,22 @@ export default {
   width: 940px;
   margin: auto;
   position: relative;
+  transition: 0.5s;
 }
 
 #corp-glide > div:last-child {
-  width: 110%;
+  width: 100%;
   position: absolute;
   display: flex;
   justify-content: space-between;
   top: 85%;
-  transform: translateY(-50%) translateX(-20px);
 }
 
 #corp-glide .glide__slides {
   display: flex;
   align-items: center;
   height: 400px;
+  padding: 0;
 }
 
 #corp-glide .glide__slide {
@@ -151,7 +191,6 @@ export default {
   padding: 0rem 1.5rem 1.5rem;
   border-radius: 5px;
   transition: all 0.3s;
-  opacity: 0.5;
 }
 
 #corp-glide .glide__slide div {
@@ -166,11 +205,16 @@ export default {
 #corp-glide .glide__slide p,
 #corp-glide .glide__slide h4 {
   margin: 0;
+  font-family: 'Nanum Gothic';
+  font-weight: 700;
+}
+
+#corp-glide .glide__slide p {
+  font-size: 12px;
 }
 
 #corp-glide .glide__slide--active {
   height: 330px;
-  opacity: 1;
 }
 
 #corp-glide .is_active > div:last-child {
@@ -189,4 +233,21 @@ export default {
   transform: perspective(1000px) rotateY(45deg);
 }
 
+@media screen and (max-width: 940px) {
+  #corp-glide {
+    width: 576px;
+  }
+
+  #corp-glide > div:last-child {
+    top: 95%;
+  }
+}
+
+@media screen and (max-width: 576px) {
+  #corp-glide {
+    width: 80%;
+    min-width: 280px;
+    max-width: 330px;
+  }
+}
 </style>
--- frontend/src/views/Explore.vue
+++ frontend/src/views/Explore.vue
@@ -5,12 +5,23 @@
 </template>
 
 <script>
+import _ from 'lodash'
 import CorporationCarousel from '@/components/explore/CorporationCarousel'
 
 export default {
   name: 'Explore',
   components: {
     CorporationCarousel,
+  },
+  data: function () {
+    return {
+      selectedKeyword: ''
+    }
+  },
+  created: function () {
+    const tags = this.$store.state.user_info.tags
+    this.selectedKeyword = _.sample(tags) ?? ''
+    console.log(this.selectedKeyword)
   }
 }
 </script>
